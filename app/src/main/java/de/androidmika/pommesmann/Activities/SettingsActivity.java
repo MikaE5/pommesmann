1c2b244648639ede71edf76df57b3c280546d23f
MikaE5 <mika.eckhardt@gmx.de>
2019-04-26 14:52:00 +0200

    settings activity

---
+++ app/src/main/java/de/androidmika/pommesmann/Activities/SettingsActivity.java
@@ -0,0 +1,15 @@
+package de.androidmika.pommesmann.Activities;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+import de.androidmika.pommesmann.R;
+
+public class SettingsActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.settings_activity);
+    }
+}
